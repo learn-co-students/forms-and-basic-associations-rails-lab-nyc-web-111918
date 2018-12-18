7b37bc04ff59112883d507a2b0ce1a55cc6e1b07
Damon Collins <damon.c.p.collins@gmail.com>
2018-12-18 16:10:35 -0500

    done

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,11 @@
 class Genre < ActiveRecord::Base
   # add associations
+  has_many :songs
+
+  def genre_ids=(ids)
+   ids.each do |id|
+     genre = Genre.find(id)
+     self.genres << genre
+   end
+ end
 end
